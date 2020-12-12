7e939dc606abc2176486d4892e75732d536b2b52
Srm-Akla <akellamahateja@gmail.com>
2020-12-12 21:24:50 +0100

    Removing files and chaning the sort order

--- configs.sh
+++ configs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-f1=~/.config/urxvt/Xresources
-f2=~/.bash/bashrc
-f3=~/.config/nvim/init.vim
-f4=~/.config/i3/config
-f5=~/.config/i3/i3status.conf
+f1=~/.config/i3/config
+f2=~/.config/urxvt/Xresources
+f3=~/.bash/bashrc
+f4=~/.config/nvim/init.vim
+f5=~/.config/i3/i3blocks/i3blocks.conf
 f6=~/.config/i3/my_i3lock.sh
 
 #3>&1 1>&2 2>&3 3>&-
@@ -12,7 +12,7 @@ clear
 
 File=$(dialog --clear \
     --title "CONFIGS" --menu "Select file" 0 0 0 \
-    1 "Xresources" 2 "Bashrc" 3 "Nvim" 4 "I3" 5 "I3 Status" 6 "I3 Lock" \
+    1 "I3" 2 "Urxvt" 3 "Bashrc" 4 "Nvim" 5 "I3 blocks" 6 "I3 Lock" \
    --stdout)
 
 clear
